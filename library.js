70b4c6fa284ed00160e48acab684909b610e80d3
AnthonyDLC <277806914+anthony-dlc@users.noreply.github.com>
2026-06-25 15:16:09 -0400

    Create Library Array and Book Object.

--- library.js
+++ library.js
@@ -2,9 +2,23 @@ const openForm = document.querySelector(".call-form");
 const formContainer = document.querySelector(".form-container");
 const closeForm = document.querySelector(".close-form");
 
-openForm.addEventListener("click", ()=>{
-  formContainer.style.display = "flex"
-})
+const library = [];
+
+function Book(author, title, pages, readIt, id) {
+  ((this.author = author),
+    (this.title = title),
+    (this.pages = pages),
+    (this.readIt = readIt),
+    (this.id = crypto.randomUUID()));
+}
+
+const AtomicHabist = new Book("James Clear", "Atomic Habits", 320, "yes");
+
+const doIt = new Book("Anthony de la cruz", "Do it", 300, "yes");
+
+openForm.addEventListener("click", () => {
+  formContainer.style.display = "flex";
+});
 
 closeForm.addEventListener("click", () => {
   formContainer.style.display = "none";
