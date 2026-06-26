ffef998286b96566addfc042a4a51a3e41a8e1a5
AnthonyDLC <277806914+anthony-dlc@users.noreply.github.com>
2026-06-26 13:17:23 -0400

    Add functions closeForm and addNewBook

--- library.js
+++ library.js
@@ -1,8 +1,17 @@
 const openForm = document.querySelector(".call-form");
 const formContainer = document.querySelector(".form-container");
-const closeForm = document.querySelector(".close-form");
+const closeF = document.querySelector(".close-form");
+const addBook = document.querySelector(".add-book");
+const bookAuthor = document.querySelector("#book-author");
+const bookTitle = document.querySelector("#book-title");
+const bookPages = document.querySelector("#book-pages");
+const readItOrNot = document.querySelectorAll(".book-read");
 
 const library = [];
+
+function closeForm() {
+  formContainer.style.display = "none";
+}
 
 function Book(author, title, pages, readIt, id) {
   ((this.author = author),
@@ -12,14 +21,35 @@ function Book(author, title, pages, readIt, id) {
     (this.id = crypto.randomUUID()));
 }
 
-const AtomicHabist = new Book("James Clear", "Atomic Habits", 320, "yes");
+function addNewBook() {
+  const author = bookAuthor.value;
+  const title = bookTitle.value;
+  const pages = bookPages.value;
+  let read;
+  readItOrNot.forEach((r) => {
+    if (r.checked) {
+      read = r.value;
+    }
+  });
 
-const doIt = new Book("Anthony de la cruz", "Do it", 300, "yes");
+  const newBook = new Book(author, title, pages, read);
+  library.push(newBook);
+}
+
+// const AtomicHabist = new Book("James Clear", "Atomic Habits", 320, "yes");
+
+// const doIt = new Book("Anthony de la cruz", "Do it", 300, "yes");
 
 openForm.addEventListener("click", () => {
   formContainer.style.display = "flex";
 });
 
-closeForm.addEventListener("click", () => {
-  formContainer.style.display = "none";
+closeF.addEventListener("click", () => {
+  closeForm();
+});
+
+addBook.addEventListener("click", () => {
+  addNewBook();
+  console.log(library);
+  // closeForm();
 });
