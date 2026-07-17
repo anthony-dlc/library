6c378c678b9a9c5d2ee0d0f12008e7d2c8074605
AnthonyDLC <277806914+anthony-dlc@users.noreply.github.com>
2026-07-17 14:10:33 -0400

    Change Structure from Basic Object to Classes.

--- library.js
+++ library.js
@@ -1,152 +1,176 @@
-const openForm = document.querySelector(".call-form");
-const formContainer = document.querySelector(".form-container");
-const closeF = document.querySelector(".close-form");
-const buttonAddBook = document.querySelector(".add-book");
-const bookAuthor = document.querySelector("#book-author");
-const bookTitle = document.querySelector("#book-title");
-const bookPages = document.querySelector("#book-pages");
-const readItOrNot = document.querySelectorAll(".book-read");
-const tableOfBooks = document.querySelector(".read-it");
-const tablesBody = document.querySelector("tbody");
-const tableRow = document.querySelector(".book-details");
-const tableData = document.querySelectorAll("td");
-
 const library = [];
 
-function Book(author, title, pages, readIt) {
-  ((this.author = author),
-    (this.title = title),
-    (this.pages = pages),
-    (this.readIt = readIt),
-    (this.id = crypto.randomUUID()));
-}
-
-openForm.addEventListener("click", () => {
-  formContainer.style.display = "flex";
-});
-
-closeF.addEventListener("click", () => {
-  closeForm();
-});
-
-buttonAddBook.addEventListener("click", () => {
-  if (
-    bookAuthor.value.trim() !== "" &&
-    bookTitle.value.trim() !== "" &&
-    bookPages.value.trim() !== ""
-  ) {
-    addNewBook();
-    alert(`${bookTitle.value} was successfully added`);
-    clearCampus();
-    showBook();
-  } else {
-    alert("Fill all campus");
+class Book {
+  constructor(author, title, pages, status) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.status = status;
+    this.id = crypto.randomUUID();
   }
-});
 
-function closeForm() {
-  formContainer.style.display = "none";
-}
+  #verifyBook() {
+    if (
+      this.author === undefined ||
+      this.title === undefined ||
+      this.pages === undefined ||
+      this.status === undefined
+    )
+      throw new Error("Fill all campus");
+  }
 
-function clearCampus() {
-  bookAuthor.value = "";
-  bookTitle.value = "";
-  bookPages.value = "";
-}
+  addBook() {
+    this.#verifyBook();
+    library.push({ ...this });
+  }
 
-function addNewBook() {
-  const author = bookAuthor.value;
-  const title = bookTitle.value;
-  const pages = bookPages.value;
-  let read;
-  readItOrNot.forEach((r) => {
-    if (r.checked) {
-      read = r.value;
-    }
-  });
-
-  const newBook = new Book(author, title, pages, read);
-  library.push(newBook);
-}
-
-function eliminateBookFromLibrary(btn) {
-  btn.addEventListener("click", () => {
-    const bookDisplayContainer = btn.closest("tr");
-    const bookIndex = library.findIndex(
-      (book) => book.id === bookDisplayContainer.id,
-    );
-    const hasIt = library.find((book) => book.id === bookDisplayContainer.id);
-
-    if (hasIt) {
+  eliminateBook() {
+    const bookIndex = library.findIndex((i) => i.id === this.id);
+    if (bookIndex !== -1) {
       library.splice(bookIndex, 1);
-      bookDisplayContainer.remove();
-    }
-  });
-}
-
-function changeBookStatus(btn, state) {
-  btn.addEventListener("click", () => {
-    const bookDisplayContainer = btn.closest("tr");
-    const bookFound = library.find(
-      (book) => book.id === bookDisplayContainer.id,
-    );
-    if (bookFound.readIt === "yes") {
-      bookFound.readIt = "no";
-      state.textContent = bookFound.readIt;
     } else {
-      bookFound.readIt = "yes";
-      state.textContent = bookFound.readIt;
+      console.error("Book not found");
     }
-    console.log(bookFound.id);
+  }
+
+  changeBookStatus() {
+    const bookIndex = library.findIndex((i) => i.id === this.id);
+    if (library[bookIndex].status === "read it") {
+      library[bookIndex].status = "Not read it";
+    } else {
+      library[bookIndex].status = "read it";
+    }
+  }
+}
+
+const displayControls = (() => {
+  const formContainer = document.querySelector(".form-container");
+  const bookAuthor = document.querySelector("#book-author");
+  const bookTitle = document.querySelector("#book-title");
+  const bookPages = document.querySelector("#book-pages");
+  const bookStatus = document.querySelectorAll(".book-status");
+  const bookContainer = document.querySelector(".books-container");
+  const btnCreateBook = document.querySelector(".create-book");
+  const btnCallForm = document.querySelector(".call-form");
+  const btnCloseForm = document.querySelector(".close-form");
+
+  const getStatusValues = () => {
+    let status;
+    bookStatus.forEach((radio) => {
+      if (radio.checked) {
+        status = radio.value;
+      }
+    });
+    return status;
+  };
+
+  const emptyInputs = () => {
+    bookAuthor.value = "";
+    bookTitle.value = "";
+    bookPages.value = "";
+  };
+
+  const addBookToLibrary = () => {
+    const statusValue = getStatusValues();
+    const newBook = new Book(
+      bookAuthor.value,
+      bookTitle.value,
+      bookPages.value,
+      statusValue,
+    );
+    newBook.addBook();
+    console.log(library);
+  };
+
+  const createDisplayBook = () => {
+    const bookInArray = library.at(-1);
+
+    const bookCard = document.createElement("div");
+    const svgContainer = document.createElement("div");
+    const author = document.createElement("p");
+    const title = document.createElement("p");
+    const pages = document.createElement("p");
+    const status = document.createElement("p");
+    const buttonsContainer = document.createElement("div");
+    const btnEliminate = document.createElement("button");
+    const btnChangeStatus = document.createElement("button");
+
+    bookCard.classList.add("book");
+    bookCard.id = bookInArray.id;
+    svgContainer.classList.add("svg-container");
+    buttonsContainer.classList.add("buttons-container");
+
+    buttonsContainer.append(btnEliminate);
+    buttonsContainer.append(btnChangeStatus);
+
+    svgContainer.innerHTML = `
+      <svg class="father-svg">
+        <use href="#book-icon" />
+      </svg>`;
+    author.textContent = `Author: ${bookInArray.author}`;
+    title.textContent = `Title: ${bookInArray.title}`;
+    pages.textContent = `Pages: ${bookInArray.pages}`;
+    status.textContent = `Status: ${bookInArray.status}`;
+    btnEliminate.textContent = "Eliminate";
+    btnChangeStatus.textContent = "Change Status";
+
+    btnEliminate.addEventListener("click", () => {
+      const bookIndex = library.findIndex(
+        (bookId) => bookId.id === bookCard.id,
+      );
+      library.splice(bookIndex, 1);
+      bookCard.remove();
+      console.log(library);
+    });
+
+    btnChangeStatus.addEventListener("click", () => {
+      if (bookInArray.status === "Read it") {
+        bookInArray.status = "Not read it";
+        status.textContent = `Status: ${bookInArray.status}`;
+      } else {
+        bookInArray.status = "Read it";
+        status.textContent = `Status: ${bookInArray.status}`;
+      }
+    });
+
+    bookCard.append(svgContainer);
+    bookCard.append(author);
+    bookCard.append(title);
+    bookCard.append(pages);
+    bookCard.append(status);
+    bookCard.append(buttonsContainer);
+
+    bookContainer.append(bookCard);
+  };
+
+  const verifyInputs = () => {
+    const authorVerifyValue = bookAuthor.value.trim() === "";
+    const titleVerifyValue = bookTitle.value.trim() === "";
+    const pagesVerifyValue = bookPages.value.trim() === "";
+    if (authorVerifyValue || titleVerifyValue || pagesVerifyValue) {
+      alert("Fill all campus");
+      return false;
+    } else {
+      formContainer.style.display = "none";
+      return true;
+    }
+  };
+
+  btnCloseForm.addEventListener("click", () => {
+    emptyInputs();
+    formContainer.style.display = "none";
   });
-}
 
-function addClassAndTextContent(el, className, text) {
-  el.classList.add(`${className}`);
-  el.textContent = `${text}`;
-}
+  btnCallForm.addEventListener("click", () => {
+    formContainer.style.display = "grid";
+  });
 
-function appendingChild(fatherName, sonName) {
-  fatherName.append(sonName);
-}
-
-function showBook() {
-  const lastBook = library.at(-1);
-
-  const bookDescription = document.createElement("tr");
-  const authorAndTitle = document.createElement("td");
-  const numberOfPages = document.createElement("td");
-  const status = document.createElement("td");
-  const changeStatus = document.createElement("td");
-  const eliminateBook = document.createElement("td");
-  const changeStatusButton = document.createElement("button");
-  const eliminateBookButton = document.createElement("button");
-
-  bookDescription.style.display = "table-row";
-
-  bookDescription.id = `${lastBook.id}`;
-
-  addClassAndTextContent(
-    authorAndTitle,
-    "author-title",
-    `${lastBook.title} by ${lastBook.author}`,
-  );
-  addClassAndTextContent(numberOfPages, "pages", `${lastBook.pages}`);
-  addClassAndTextContent(status, "status", `${lastBook.readIt}`);
-  addClassAndTextContent(changeStatusButton, "change-status", "Change Status");
-  addClassAndTextContent(eliminateBookButton, "eliminate", "Eliminate");
-
-  appendingChild(changeStatus, changeStatusButton);
-  appendingChild(eliminateBook,eliminateBookButton);
-  appendingChild(bookDescription, authorAndTitle);
-  appendingChild(bookDescription,numberOfPages);
-  appendingChild(bookDescription,status);
-  appendingChild(bookDescription, changeStatus);
-  appendingChild(bookDescription,eliminateBook);
-  appendingChild(tablesBody,bookDescription);
-  appendingChild(tableOfBooks, tablesBody);
-
-
-  changeBookStatus(changeStatusButton, status);
-  eliminateBookFromLibrary(eliminateBookButton);
-}
+  btnCreateBook.addEventListener("click", () => {
+    const verifyInputsResult = verifyInputs();
+    if (verifyInputsResult === true) {
+      addBookToLibrary();
+      createDisplayBook();
+      emptyInputs();
+    }
+  });
+})();
